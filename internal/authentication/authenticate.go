b4b5b0fce7c4a13fa685465fe765f02dbdb93fd2
Milan Gowin <milan.gowin@gmail.com>
2021-01-05 00:57:56 +0100

    Update authenticate.go

--- internal/authentication/authenticate.go
+++ internal/authentication/authenticate.go
@@ -4,25 +4,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-// type CloudSettings struct {
-// 	AwsProfile string
-// 	AwsRegion  string
-// }
-
-// var AwsSettings CloudSettings
-
-// //AWS Authenticate
-// func Amazon() {
-
-// 	AwsSettings.AwsProfile = viper.GetString("aws.profile")
-// 	AwsSettings.AwsRegion = viper.GetString("aws.region")
-
-// }
-
-//below is a bit over-kill. Commented out way for now is better
-//but it could be that below is future-proofed for more robust authentication
-//if we come to it.
-
 //Authenticator
 type Authenticator interface {
 	initialize()
